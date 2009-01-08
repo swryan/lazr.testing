7ace5b6bb96a38909bde9a16e22812bad5e7e45f
Barry Warsaw <barry@python.org>
2009-01-08 13:44:42 -0500

    Initial package template for lazr packages.

---
+++ setup.py
@@ -0,0 +1,62 @@
+# Copyright 2009 Canonical Ltd.  All rights reserved.
+#
+# This file is part of lazr.yourpkg
+#
+# lazr.config is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# lazr.config is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with lazr.config.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup, find_packages
+
+# generic helpers primarily for the long_description
+def generate(*docname_or_string):
+    res = []
+    for value in docname_or_string:
+        if value.endswith('.txt'):
+            f = open(value)
+            value = f.read()
+            f.close()
+        res.append(value)
+        if not value.endswith('\n'):
+            res.append('')
+    return '\n'.join(res)
+# end generic helpers
+
+from lazr.yourpkg import __version__
+
+setup(
+    name='lazr.yourpkg',
+    version='1.1',
+    namespace_packages=['lazr'],
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    include_package_data=True,
+    zip_safe=False,
+    maintainer='LAZR Developers',
+    maintainer_email='lazr-developers@lists.launchpad.net',
+    description=open('README.txt').readline().strip(),
+    long_description=generate(
+        'src/lazr/yourpkg/README.txt',
+        'src/lazr/yourpkg/NEWS.txt'),
+    license='LGPL v3',
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+        ],
+    url='https://launchpad.net/lazr.yourpkg',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python"],
+    )
---
+++ src/lazr/yourpkg/__init__.py
@@ -0,0 +1,26 @@
+# Copyright 2009 Canonical Ltd.  All rights reserved.
+#
+# This file is part of lazr.yourpkg
+#
+# lazr.config is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# lazr.config is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with lazr.config.  If not, see <http://www.gnu.org/licenses/>.
+
+__version__ = '0.1'
+
+# This is a namespace package.
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
